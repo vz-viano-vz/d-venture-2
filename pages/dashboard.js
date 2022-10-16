c9d792bde76697102662833aa3816bdfad4061e1
Octaviano <215232220@students.uii.ac.id>
2022-10-16 22:37:03 +0700

    protected route

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,13 @@
+import { router } from "next/router";
 import React from "react";
 import { useAuth } from "../context/AuthContext";
 
 const dashboard = () => {
     const currentUser = useAuth()
     console.log(currentUser)
-
+    if(!currentUser.isAuthenticated){
+        router.push('/login')
+    }
     return ( 
         <div>Hello </div>
     );
