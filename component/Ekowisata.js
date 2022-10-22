359e8f117d438de480f9771327f861f24da7e09b
agungprasetyosaya <aprasetyo6911@gmail.com>
2022-10-22 23:40:49 +0700

    tambahin destinasi

--- component/Ekowisata.js
+++ component/Ekowisata.js
@@ -1,30 +1,77 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-const Map = dynamic(() =>import("../component/map/Map"),{ssr:false});
+import React from 'react';
+import dynamic from 'next/dynamic';
+const Map = dynamic(() => import('../component/map/Map'), { ssr: false });
 export default function Ekowisata() {
   return (
     <div>
-    <nav class="navbar bg-light">
+      {/* <nav class="navbar bg-light">
         <div class="container-fluid">
                 <a class="navbar-brand" href="#">&lt; Dashboard</a>
         </div>
-    </nav>
-    <div className='container-fluid'>
-        <div className='row'>
-            <div className='col col-lg-3'>
-                <div class="card" style={{width: "18rem"}}>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+    </nav> */}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-lg-3">
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Wana Tirta Mangrove Forest</h5>
+                <p class="card-text">Kulon progo</p>
+                <a href="https://goo.gl/maps/M1MyJiu7qV6dvzPZ6" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
-            <div className='col col-lg-9'>
-                <Map/>
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Taman Sungai Mudal</h5>
+                <p class="card-text">Kulon progo</p>
+                <a href="https://goo.gl/maps/XBnPC2xgCcnEH6NU8" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Umbul SidoMukti</h5>
+                <p class="card-text">Kulon progo</p>
+                <a href="https://goo.gl/maps/T7gPqQvF2JH4p7DY9" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Wisata Jatimulyo</h5>
+                <p class="card-text">Kulon progo</p>
+                <a href="https://goo.gl/maps/ky3pXtNTgYnGBuo98" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Mangrove Jembatan api</h5>
+                <p class="card-text">Kulon progo</p>
+                <a href="https://goo.gl/maps/jM5v71uWj3CW6QQw5" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div class="card my-2" style={{ width: '18rem' }}>
+              <div class="card-body">
+                <h5 class="card-title">Desa Ekowisata Pancoh</h5>
+                <p class="card-text">Sleman</p>
+                <a href="https://goo.gl/maps/VVUBU8uapp1Mifvc9" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col col-lg-9">
+            <Map />
+          </div>
         </div>
+      </div>
     </div>
-</div>
-  )
+  );
 }
--- pages/ekowisata.js
+++ pages/ekowisata.js
@@ -1,14 +1,18 @@
-import React from 'react'
-
-import Navdash from '../component/Navdash'
-import dynamic from 'next/dynamic'
-const Ekowisata = dynamic(() =>import("../component/Ekowisata"),{ssr:false});
-
+import React from 'react';
+import styles from '../styles/Home.module.css';
+import Navdash from '../component/Navdash';
+import dynamic from 'next/dynamic';
+const Ekowisata = dynamic(() => import('../component/Ekowisata'), { ssr: false });
 
 export default function ekowisata() {
   return (
     <div>
-        <Ekowisata/>
+      <div class={styles.bgblue}>
+        <Navdash />
+        <div class="rounded">
+          <Ekowisata />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
