19e5f1037136c585828a188aad45c9ae86822328
agungprasetyosaya <aprasetyo6911@gmail.com>
2022-10-22 22:38:46 +0700

    Merge branch 'main' of https://github.com/vianozx/D-aventure

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,23 +3,28 @@ import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import styles from '../../styles/Home.module.css';
 import Navdash from '../../component/Navdash';
+const Map = dynamic(() => import('../../component/map/Map'), { ssr: false });
 
-
-const dashboard = () => {
+export default dashboard = () => {
   const currentUser = useAuth();
   console.log(currentUser);
 
   return (
     <div>
       <div class={styles.bgblue}>
-        <div class={styles.container}>
+        <div class={styles.containerx}>
           <Navdash />
           <h1 class="text-center">Eco Wisata</h1>
-          
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col col-lg-3">hello</div>
+              <div className="col col-lg-9">
+                <Map />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
-export default dashboard;
--- pages/ekowisata.js
+++ pages/ekowisata.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-import Navdash from '../component/Navdash'
+import React from 'react';
+import dynamic from 'next/dynamic';
+import Navdash from '../component/Navdash';
+import Image from 'next/image';
+import gunung1 from '../assets/gunung1.png';
 
-const Map = dynamic(() =>import("../component/map/Map"),{ssr:false});
-
+// const Map = dynamic(() => import('../component/map/Map'), { ssr: false });
 
 export default function ekowisata() {
   return (
     <div>
-        <nav class="navbar bg-light">
-            <div class="container-fluid">
-                    <a class="navbar-brand" href="#">&lt; Dashboard</a>
-            </div>
-        </nav>
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col col-lg-3'>
-                    hello
-                </div>
-                <div className='col col-lg-9'>
-                    <Map/>
-                </div>
-            </div>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            &lt; Dashboard
+          </a>
         </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-lg-3">
+            <div class="card" style="width: 18rem;">
+              {/* <Image src={gunung1} class="card-img-top" alt="" /> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col col-lg-9">{/* <Map /> */}</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
