b925b4b9eb3d257b4311c443da8d1499dc018fe5
Octaviano <215232220@students.uii.ac.id>
2022-10-22 21:00:31 +0700

    annjing

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import styles from '../../styles/Home.module.css';
 import Navdash from '../../component/Navdash';
-import Map from '../../component/map';
+
 
 const dashboard = () => {
   const currentUser = useAuth();
@@ -15,9 +15,6 @@ const dashboard = () => {
         <div class={styles.container}>
           <Navdash />
           <h1 class="text-center">Eco Wisata</h1>
-        </div>
-        <div>
-          <Map/>
         </div>
       </div>
     </div>
---
+++ pages/ekowisata.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Map from '../component/map/Map'
+import Navdash from '../component/Navdash'
+export default function ekowisata() {
+  return (
+    <div>
+        <Navdash/>
+        <Map/>
+    </div>
+  )
+}
