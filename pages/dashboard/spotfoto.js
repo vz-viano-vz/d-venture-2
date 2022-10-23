17d1763023d125b9140eeb5e4cdb346a0c4e5303
agungprasetyosaya <aprasetyo6911@gmail.com>
2022-10-23 09:44:36 +0700

    asdasd

--- pages/dashboard/spotfoto.js
+++ pages/dashboard/spotfoto.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
 import Navdash from '../../component/Navdash';
+import Image from 'next/image';
 
 const spotfoto = () => {
   return (
@@ -10,6 +11,15 @@ const spotfoto = () => {
         <div class={styles.containerx}>
           <Navdash />
           <h1 class="text-center">Spot Foto</h1>
+          <div class="">
+            <div class="card" style="width: 18rem;">
+              <Image src="" class="card-img-top" alt="" />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Wana Tirta Mangrove Forest</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
